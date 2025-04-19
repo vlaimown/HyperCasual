e5f21bfc84d38e2620c7836c8f8cd20515e7f49f
Kysska <saifutdinovaila514@gmail.com>
2025-04-19 16:45:18 +1000

    end_screen, ads
    Добавлен экран Победа, поражение и реклама

---
+++ Assets/EndLevelScreen.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class EndLevelScreen : MonoBehaviour
+{
+    public GameObject adsDialog;
+    public GameObject buttonAds;
+    public GameObject buttonCancel;
+    public GameObject textAds;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReloadLevel()
+    {
+        Debug.Log("перезагрузка сцены");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("переход в главное меню");
+        hideDiaolog();
+    }
+
+    public void NextLevel()
+    {
+        Debug.Log("переход на следующий уровень");
+    }
+
+    public void showDiaolog()
+    {
+        gameObject.SetActive(true);
+        buttonAds.SetActive(true);
+    }
+
+    public void hideDiaolog()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void showAds()
+    {
+        StartCoroutine(ShowDialogForSeconds());
+        buttonAds.SetActive(false);
+    }
+
+    private IEnumerator ShowDialogForSeconds()
+    {
+        TMP_Text text = textAds.GetComponent<TMP_Text>();
+        adsDialog.SetActive(true);
+        buttonCancel.SetActive(false);
+        for (int i = 3; i >= 0; i--) 
+        {
+            text.text = i + "s"; 
+            yield return new WaitForSeconds(1f);
+        }
+        textAds.SetActive(false);
+        buttonCancel.SetActive(true);
+    }
+
+    public void onClickCancelAdsButton()
+    {
+        adsDialog.SetActive(false);
+    }
+}
